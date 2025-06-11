15139432f62fbb69c04f5bae781c201e034fd411
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-06-11 10:42:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PrenotaLab_web
+Permette agli studenti di prenotare postazioni PC e ai docenti di organizzare lezioni nei laboratori.
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected!"))
+  .catch(err => console.log(err));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
