24a3415886d8a8165e2993f29012790b564755f3
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-06-15 10:55:50 +0200

    database connected successfully

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected!"))
   .catch(err => console.log(err));
 
